25940a617d3b8af06a60b4ef2b9d83ac18880e9c
medicaless <danil00@naver.com>
2025-03-27 12:22:13 +0900

    v0.7 week04_prime.py change

--- week04_prime.py
+++ week04_prime.py
@@ -6,11 +6,10 @@ def is_prime(number):
 
     if (number >= 2):
         i = 2
-        while (i * i <= number):
-            # for i in range(2, number):
+        for i in range(2, int(number**0.5)+1):
+            # while( i * i <= number):
             if number % i == 0:
                 return False
-            #print(i, end=" ")
             i = i + 1
     else:
         return False
