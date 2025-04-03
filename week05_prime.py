35c3cbaf9dad1251c4ed03c1ef79675ee12beb70
medicaless <danil00@naver.com>
2025-04-03 12:23:34 +0900

    0.9 week05_prime add

---
+++ week05_prime.py
@@ -0,0 +1,25 @@
+def is_prime(number:int) -> bool:
+    """
+    decimal determinant variable
+    """
+    is_prime = True
+
+    if (number >= 2):
+        i = 2
+        for i in range(2, int(number**0.5)+1):
+            # while( i * i <= number):
+            if number % i == 0:
+                return False
+            i = i + 1
+    else:
+        return False
+    return True
+
+n1 = int(input())
+n2 = int(input())
+
+
+
+for i in range(n1, n2+1):
+    if is_prime(i):
+        print(i, end=' ')
