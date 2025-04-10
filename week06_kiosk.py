576f46c51a5ddc78ce612c8b48beb32148928148
medicaless <danil00@naver.com>
2025-04-10 11:39:50 +0900

    ver x.9 kiosk update

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,9 +1,21 @@
 # 아아 : 2000 # 라떼 2500
 
+def order_process(idx):
+    global total_price
+    """
+    주문 처리 함수 1) 주문 디스플레이 2)총 주문 금액 누산 3)수량 업데이트
+    :return: 없음
+    """
+    print(f"{drinks[idx]}를 선택하셨습니다. 가격은 {prices[idx]}원 입니다.")
+    total_price += prices[idx]
+    amounts[idx] += 1
+
+
+total_price = 0
+
 drinks = ["아이스 아메리카노", "카페 라떼", "수박 주스"]
 prices = [2000, 2500, 3000]
 amounts = [0,0,0]
-total_price = 0
 
 menu_texts = ""
 
@@ -12,20 +24,10 @@ for j in range(len(drinks)):
 menu_texts = menu_texts + f"{len(drinks)+1})주문 종료 :"
 
 while(True):
-    menu = input(menu_texts)
-    if menu == "1":
-        print(f"{drinks[0]}를 선택하셨습니다. 가격은 {prices[0]}원 입니다.")
-        total_price += prices[0]
-        amounts[0] += 1
-    elif menu == "2":
-        print(f"{drinks[1]}를 선택하셨습니다. 가격은 {prices[1]}원 입니다.")
-        total_price += prices[1]
-        amounts[1] += 1
-    elif menu == "3":
-        print(f"{drinks[2]}를 선택하셨습니다. 가격은 {prices[2]}원 입니다.")
-        total_price += prices[2]
-        amounts[2] += 1
-    elif menu == "4":
+    menu = int(input(menu_texts))
+    if len(drinks) >= menu >= 1:
+        order_process(menu-1)
+    elif menu == len(drinks)+1:
         print("주문을 종료합니다.")
         break
     else:
