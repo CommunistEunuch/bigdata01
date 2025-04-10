28467e8d2c116c5aa2f3c9e6bc11e59c0192b811
medicaless <danil00@naver.com>
2025-04-10 10:37:57 +0900

    ver x.3 kiosk update

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,13 +1,17 @@
 # 아아 : 2000 # 라떼 2500
 prices = [2000, 2500]
 drinks = ["아이스 아메리카노", "카페 라떼"]
-
+total_price = 0
 while(True):
-    menu = input(f"1) {drinks[0]} {prices[0]}원 2) {drinks[1]} {prices[1]}원 3) 주문 종료")
+    menu = input(f"1) {drinks[0]} {prices[0]}원 2) {drinks[1]} {prices[1]}원 3) 주문 종료 : ")
     if menu == "1":
         print(f"{drinks[0]}를 선택하셨습니다. 가격은 {prices[0]}원 입니다.")
+        total_price += prices[0]
     elif menu == "2":
         print(f"{drinks[1]}를 선택하셨습니다. 가격은 {prices[1]}원 입니다.")
+        total_price += prices[1]
     elif menu == "3":
         print("주문을 종료합니다.")
         break
+    else:
+        print(f"{menu}메뉴는 존재하지 않습니다. 다시 시도해주세요.")
