ec65bb8696c0b3e410682606fdf99cf374beb534
medicaless <danil00@naver.com>
2025-04-18 11:08:20 +0900

    2.2 kiosk09 update -from

--- week09_kiosk.py
+++ week09_kiosk.py
@@ -1,18 +1,18 @@
 # 아아 : 2000 # 라떼 2500
 import sys
-import kiosk as kk
-
+#import kiosk as kk
+from kiosk import *
 
 while(True):
     try:
-        menu = int(input(kk.display_menu()))
-        if len(kk.drinks) >= menu >= 1:
-            kk.order_process(menu-1)
-        elif menu == len(kk.drinks)+1:
+        menu = int(input(display_menu()))
+        if len(drinks) >= menu >= 1:
+            order_process(menu-1)
+        elif menu == len(drinks)+1:
             print("주문을 종료합니다.")
             break
         else:
             print(f"{menu}메뉴는 존재하지 않습니다. 다시 시도해주세요.")
     except ValueError:
         print("문자를 입력할 수 없습니다. 다시 시도해주세요")
-kk.print_receipt()
+print_receipt()
