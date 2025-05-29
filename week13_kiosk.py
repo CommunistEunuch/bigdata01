b4307f2380b6ae723aeb93a81aa361e238eead8e
medicaless <danil00@naver.com>
2025-05-29 10:33:37 +0900

    v3.6 week13_kiosk.py add

---
+++ week13_kiosk.py
@@ -0,0 +1,10 @@
+import kiosk as kk
+import requests
+
+if __name__ == "__main__":
+    url = ("http://wttr.in/suwon?format=%C++%t&lang=ko")
+    response = requests.get(url)
+    print(response.text.strip())
+    kk.run()
+    kk.print_receipt()
+    kk.print_ticket_number()
