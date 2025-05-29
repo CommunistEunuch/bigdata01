e1b1c321aeaaf2a6f74650bb182febc213e1c20e
medicaless <danil00@naver.com>
2025-05-29 11:45:11 +0900

    3.9 kiosk.py update

--- kiosk.py
+++ kiosk.py
@@ -1,11 +1,12 @@
 import datetime as datetime
 import sqlite3
+import requests
 
 drinks = ["아이스 아메리카노", "카페 라떼", "수박 주스", "딸기 주스"]
 prices = [2000, 2500, 3000, 3200]
 amounts = [0] * len(drinks) #오히려 현재 코드에서는 이쪽이 더 빠를 수 있음.
-
-
+# url = ("http://wttr.in/suwon?format=%C++%t&lang=ko")
+url = ("https://wttr.in/suwon?format=2")
 total_price = 0
 
 #할인 적용 정책 : 원
@@ -49,6 +50,15 @@ def display_menu() -> str:
     음료 선택 메뉴 디스플레이 함수
     :return: 음료 메뉴 및 주문 종료 문자열 (문자열)
     """
+    try:
+        response = requests.get(url)
+        if response.status_code == 200 :
+            print(response.text.strip())
+        else :
+            print(f"상태 코드 {response.status_code}")
+
+    except Exception as e :
+        print(e)
     print("="*30)
     menu_texts =''.join(f"{j+1}) {drinks[j]} {prices[j]}원\n " for j in range(len(drinks)))
     menu_texts = menu_texts + f"{len(drinks)+1} 주문 종료 : "
